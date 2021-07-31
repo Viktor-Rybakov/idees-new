8a4c85170999fa11842e939cb4a7119d41ca2f36
Viktor Rybakov <viktor.rybakov@gmail.com>
2021-07-31 14:05:51 +0300

    добавляет меню, исправляет шаблоны

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,9 +4,10 @@ import setFormValidation from './form-validation.js';
 import setModal from './modal.js';
 
 document.addEventListener('DOMContentLoaded', () => {
+  setModal('.modal', '.form-section', '.js-form-open', '.js-form-close');
+  setModal('.modal', '.menu', '.js-menu-open', '.js-menu-close');
+
   checkCookiesAccept();
   mailingSubscribe();
   setFormValidation();
-
-  setModal('.modal', '.form-section', '.js-form-open', '.js-form-close');
 });
