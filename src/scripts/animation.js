ef5ada1ee1fc79a3856a0c694fd86c57a06dd4d1
Viktor Rybakov <viktor.rybakov@gmail.com>
2021-09-13 23:17:55 +0300

    Испрвляет ошибки

--- src/scripts/animation.js
+++ src/scripts/animation.js
@@ -1,18 +1,21 @@
 export default function setAnimation() {
-  const animationEelem = document.querySelector('.main-section__scene-img');
+  const animationElem = document.querySelector('.main-section__scene-img');
   const animationTarget = document.querySelector('.animation-target');
 
-  animationTarget.x = animationTarget.offsetLeft + animationTarget.clientWidth / 2 - animationEelem.clientWidth / 2;
-  animationTarget.y = animationTarget.offsetTop + animationTarget.clientHeight / 2 - animationEelem.clientHeight / 2;
+  if (animationElem && animationTarget) {
 
-  setElemPosition(animationEelem, animationTarget.x, animationTarget.y);
+    animationTarget.x = animationTarget.offsetLeft + animationTarget.clientWidth / 2 - animationElem.clientWidth / 2;
+    animationTarget.y = animationTarget.offsetTop + animationTarget.clientHeight / 2 - animationElem.clientHeight / 2;
 
-  window.addEventListener('resize', () => {
-    animationTarget.x = animationTarget.offsetLeft + animationTarget.clientWidth / 2 - animationEelem.clientWidth / 2;
-    animationTarget.y = animationTarget.offsetTop + animationTarget.clientHeight / 2 - animationEelem.clientHeight / 2;
+    setElemPosition(animationElem, animationTarget.x, animationTarget.y);
 
-    setElemPosition(animationEelem, animationTarget.x, animationTarget.y);
-  });
+    window.addEventListener('resize', () => {
+      animationTarget.x = animationTarget.offsetLeft + animationTarget.clientWidth / 2 - animationElem.clientWidth / 2;
+      animationTarget.y = animationTarget.offsetTop + animationTarget.clientHeight / 2 - animationElem.clientHeight / 2;
+
+      setElemPosition(animationElem, animationTarget.x, animationTarget.y);
+    });
+  }
 
   function setElemPosition (elem, x, y) {
     elem.style.setProperty('--position-x', `${x}px`);
