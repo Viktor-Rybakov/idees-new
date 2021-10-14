9a3405cebd0ae7de10552e37904e42804653b52a
Viktor Rybakov <viktor.rybakov@gmail.com>
2021-10-14 23:01:39 +0300

    Исправляет анимацию

--- src/scripts/theme.js
+++ src/scripts/theme.js
@@ -1,48 +1,67 @@
 export default function setThemeButton() {
   const body = document.querySelector('body');
   const themeButtons = body.querySelectorAll('.js-theme');
+  let isAnimationEnabled = true;
 
   themeButtons.forEach((themeButton) => {
     themeButton.addEventListener('click', (evt) => {
       evt.preventDefault();
 
-      themeButton.x = themeButton.offsetLeft + themeButton.clientWidth / 2;
-      themeButton.y = themeButton.offsetTop + themeButton.clientHeight / 2;
+      if (isAnimationEnabled) {
+        isAnimationEnabled = false;
 
-      if (localStorage.getItem('darkTheme')) {
-        localStorage.removeItem('darkTheme');
+        themeButton.x = themeButton.offsetLeft + themeButton.clientWidth / 2;
+        themeButton.y = themeButton.offsetTop + themeButton.clientHeight / 2;
 
-        const animationBlock = document.createElement('div');
-        animationBlock.classList.add('dark-light-animation');
-        setElemPosition(animationBlock, themeButton.x, themeButton.y);
-        body.classList.add('light-typo');
-        body.append(animationBlock);
+        if (localStorage.getItem('darkTheme')) {
+          localStorage.removeItem('darkTheme');
 
-        setTimeout(() => {
-          body.classList.remove('dark');
-          body.classList.remove('light-typo');
-        }, 300);
+          const animationBlock = document.createElement('div');
+          animationBlock.classList.add('dark-light-animation');
+          setElemPosition(animationBlock, themeButton.x, themeButton.y);
+          body.append(animationBlock);
+          body.classList.add('light-button');
 
-        setTimeout(() => {
-          animationBlock.remove();
-        }, 500);
-      } else {
-        localStorage.setItem('darkTheme', true);
+          setTimeout(() => {
+            body.classList.add('light-typo');
+          }, 300);
 
-        const animationBlock = document.createElement('div');
-        animationBlock.classList.add('light-dark-animation');
-        setElemPosition(animationBlock, themeButton.x, themeButton.y);
-        body.classList.add('dark-typo');
-        body.append(animationBlock);
+          setTimeout(() => {
+            body.classList.remove('dark');
+            body.classList.remove('light-typo');
+            body.classList.remove('light-button');
 
-        setTimeout(() => {
-          body.classList.add('dark');
-          body.classList.remove('dark-typo');
-        }, 300);
+          }, 1300);
 
-        setTimeout(() => {
-          animationBlock.remove();
-        }, 500);
+          setTimeout(() => {
+            animationBlock.remove();
+            isAnimationEnabled = true;
+          }, 1500);
+        } else {
+          localStorage.setItem('darkTheme', true);
+
+          const animationBlock = document.createElement('div');
+          animationBlock.classList.add('light-dark-animation');
+          setElemPosition(animationBlock, themeButton.x, themeButton.y);
+          body.append(animationBlock);
+          body.classList.add('dark-button');
+
+
+          setTimeout(() => {
+            body.classList.add('dark-typo');
+          }, 300);
+
+          setTimeout(() => {
+            body.classList.add('dark');
+            body.classList.remove('dark-typo');
+            body.classList.remove('dark-button');
+          }, 1300);
+
+          setTimeout(() => {
+            animationBlock.remove();
+            isAnimationEnabled = true;
+          }, 1500);
+        }
       }
     });
   });
